4d0619a19e4325803ee6c43c5f52ae61875db3b7
Tan Jing Yu, Denis 陈劲宇 <denis.tanjingyu@Hotmail.sg>
2020-12-14 12:38:33 +0800

    Add files via upload

---
+++ streamlit_ui.py
@@ -0,0 +1,64 @@
+"""Create an Image Classification Web App using PyTorch and Streamlit."""
+# import libraries
+from PIL import Image
+from torchvision import models, transforms
+import torch
+import streamlit as st
+
+# set title of app
+st.title("Simple Image Classification Application")
+st.write("")
+
+# enable users to upload images for the model to make predictions
+file_up = st.file_uploader("Upload an image", type = "jpg")
+
+
+def predict(image):
+    """Return top 5 predictions ranked by highest probability.
+
+    Parameters
+    ----------
+    :param image: uploaded image
+    :type image: jpg
+    :rtype: list
+    :return: top 5 predictions ranked by highest probability
+    """
+    # create a ResNet model
+    resnet = models.resnet101(pretrained = True)
+
+    # transform the input image through resizing, normalization
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean = [0.485, 0.456, 0.406],
+            std = [0.229, 0.224, 0.225]
+            )])
+
+    # load the image, pre-process it, and make predictions
+    img = Image.open(image)
+    batch_t = torch.unsqueeze(transform(img), 0)
+    resnet.eval()
+    out = resnet(batch_t)
+
+    with open('imagenet_classes.txt') as f:
+        classes = [line.strip() for line in f.readlines()]
+
+    # return the top 5 predictions ranked by highest probabilities
+    prob = torch.nn.functional.softmax(out, dim = 1)[0] * 100
+    _, indices = torch.sort(out, descending = True)
+    return [(classes[idx], prob[idx].item()) for idx in indices[0][:5]]
+
+
+if file_up is not None:
+    # display image that user uploaded
+    image = Image.open(file_up)
+    st.image(image, caption = 'Uploaded Image.', use_column_width = True)
+    st.write("")
+    st.write("Just a second ...")
+    labels = predict(file_up)
+
+    # print out the top 5 prediction labels with scores
+    for i in labels:
+        st.write("Prediction (index, name)", i[0], ",   Score: ", i[1])
